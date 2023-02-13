fde18126126f2999016b05b96f2df71fba4cae0c
Byurhan Nurula <imbyurhan@gmail.com>
2023-02-13 23:21:44 +0200

    Remove unused code

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,8 +2,6 @@ import '@/styles/globals.scss';
 import '@/styles/prism-atom-dark.scss';
 
 import Head from 'next/head';
-import Router from 'next/router';
-import nprogress from 'nprogress';
 import type { AppProps } from 'next/app';
 import { Inter } from '@next/font/google';
 import { Toaster } from 'react-hot-toast';
@@ -11,10 +9,6 @@ import { Toaster } from 'react-hot-toast';
 import { cn } from '@/utils';
 
 const inter = Inter({ subsets: ['latin'] });
-
-Router.events.on('routeChangeStart', nprogress.start);
-Router.events.on('routeChangeError', nprogress.done);
-Router.events.on('routeChangeComplete', nprogress.done);
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
